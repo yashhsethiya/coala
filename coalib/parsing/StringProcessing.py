a8a87d5ee6b0c9a49245afea91f78210427e84f4
Makman2 <mak95@arcor.de>
2015-10-17 23:38:15 +0200

    StringProcessing: Add Match class
    The Match class will be used during the StringProcessing refactor to
    return/yield objects that contain more informatino than plain strings.

--- coalib/parsing/StringProcessing.py
+++ coalib/parsing/StringProcessing.py
@@ -1,5 +1,68 @@
 import re
 
+from coalib.misc.Decorators import generate_repr, generate_eq
+
+
+@generate_repr("match", "range")
+@generate_eq("match", "position")
+class Match:
+    """
+    Stores information about a single textual match.
+    """
+
+    def __init__(self, match, position):
+        """
+        Instantiates a new Match.
+
+        :param match:    The actual matched string.
+        :param position: The position where the match was found.
+        """
+        self._match = match
+        self._position = position
+
+    def __len__(self):
+        return len(self.match)
+
+    def __str__(self):
+        return self.match
+
+    @property
+    def match(self):
+        """
+        Returns the text matched.
+
+        :returns: The text matched.
+        """
+        return self._match
+
+    @property
+    def position(self):
+        """
+        Returns the position where the text was matched.
+
+        :returns: The position.
+        """
+        return self._position
+
+    @property
+    def end_position(self):
+        """
+        Marks the end position of the matched text.
+
+        :returns: The end-position.
+        """
+        return len(self) + self.position
+
+    @property
+    def range(self):
+        """
+        Returns the position range where the text was matched.
+
+        :returns: A pair indicating the position range. The first element is
+                  the start position, the second one the end position.
+        """
+        return (self.position, self.end_position)
+
 
 def limit(iterator, count):
     """
